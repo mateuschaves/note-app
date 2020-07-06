d473d55259bbaa293f8a2bdcfcee8043a518a6e0
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-07-06 01:03:35 -0300

    Adding Context

--- App.js
+++ App.js
@@ -10,41 +10,45 @@ import CreatePostScreen from "./src/screens/CreatePostScreen";
 import EditPostScreen from "./src/screens/EditPostScreen";
 import ShowPostScreen from "./src/screens/ShowPostScreen";
 
+import { BlogProvider } from "./src/context/BlogContext";
+
 export default function App() {
   const Stack = createStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="CreatePost">
-        <Stack.Screen
-          name="Posts"
-          component={PostsScreen}
-          options={{
-            headerTitle: "Publicações",
-          }}
-        />
-        <Stack.Screen
-          name="CreatePost"
-          component={CreatePostScreen}
-          options={{
-            headerTitle: "Nova Publicação",
-          }}
-        />
-        <Stack.Screen
-          name="EditPost"
-          component={EditPostScreen}
-          options={{
-            headerTitle: "Editar Publicação",
-          }}
-        />
-        <Stack.Screen
-          name="ShowPost"
-          component={ShowPostScreen}
-          options={{
-            headerTitle: "Publicação",
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="CreatePost">
+          <Stack.Screen
+            name="Posts"
+            component={PostsScreen}
+            options={{
+              headerTitle: "Publicações",
+            }}
+          />
+          <Stack.Screen
+            name="CreatePost"
+            component={CreatePostScreen}
+            options={{
+              headerTitle: "Nova Publicação",
+            }}
+          />
+          <Stack.Screen
+            name="EditPost"
+            component={EditPostScreen}
+            options={{
+              headerTitle: "Editar Publicação",
+            }}
+          />
+          <Stack.Screen
+            name="ShowPost"
+            component={ShowPostScreen}
+            options={{
+              headerTitle: "Publicação",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
